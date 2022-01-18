c3ee5527963f0ca963b164a9e5c688162bf18d10
Sebastian Lara <pelukdev@gmail.com>
2022-01-18 13:15:08 -0300

    Example routes config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { 
+	BrowserRouter as Router,
+	Routes,
+	Route } from 'react-router-dom'
+
+import 'materialize-css/dist/css/materialize.min.css'
+
+import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+		<Navbar/>
+    <Router>
+			<Routes>
+				<Route path="/" element={<Home/>} exact/>
+				<Route path="/home" element={<Home/>} exact/>
+				<Route path="/quienes-somos" element={<Home/>} exact/>
+				<Route path="/login" element={<Home/>} exact/>
+			</Routes>
+		</Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
