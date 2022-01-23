280929e976f68587e74b05b3aede2dee93c91441
Sebastian Lara <pelukdev@gmail.com>
2022-01-23 15:18:51 -0300

    Component 404

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-import { 
-	BrowserRouter as Router,
-	Routes,
-	Route } from 'react-router-dom'
 
 import 'materialize-css/dist/css/materialize.min.css'
 
-import Home from './components/Home'
 import Navbar from './components/Navbar'
-import Login from './components/Login'
+import AppRouter from './components/AppRouter'
 
 ReactDOM.render(
   <React.StrictMode>
-		<Navbar/>
-    <Router>
-			<Routes>
-				<Route path="/" element={<Home/>} exact/>
-				<Route path="/login" element={<Login/>} exact/>
-			</Routes>
-		</Router>
+		<Navbar />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
