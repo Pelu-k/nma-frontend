a60ab18f62e7b2e74ded112d840345c67e83bd61
Sebastian Lara <pelukdev@gmail.com>
2022-01-27 16:42:31 -0300

    Loading component

---
+++ src/components/Utils/Loading/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="container center">
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="preloader-wrapper big active center">
+        <div className="spinner-layer spinner-blue-only">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div>
+          <div className="gap-patch">
+            <div className="circle"></div>
+          </div>
+          <div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
