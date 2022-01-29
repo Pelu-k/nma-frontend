5c2e918517a0d08c4b02804e22b940686f7cfb02
Sebastian Lara <pelukdev@gmail.com>
2022-01-28 21:36:54 -0300

    datepicker component

---
+++ src/components/Utils/DatePicker/index.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import M from "materialize-css";
+
+const DatePicker = ({ label, id }) => {
+  useEffect(() => {
+    const elem = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(elem);
+  });
+
+  return (
+    <div className="col s12 m4">
+      <label htmlFor={id}>{label}</label>
+      <input placeholder={label} type="text" className="datepicker" id={id} />
+    </div>
+  );
+};
+
+export default DatePicker;
