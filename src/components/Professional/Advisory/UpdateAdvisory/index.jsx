d87e441bf3b8e545d2c7dbc615ecd0d193681582
Sebastian Lara <pelukdev@gmail.com>
2022-01-29 15:52:44 -0300

    Actualizar estado de asesoria

---
+++ src/components/Professional/Advisory/UpdateAdvisory/index.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Badges from "../../../Utils/Badges";
+import Loading from "../../../Utils/Loading";
+
+const UpdateAdvisory = () => {
+  const [estado, setEstado] = useState(true);
+  const [asesorias, setAsesorias] = useState();
+
+  const obtenerAsesorias = async (idProfesional) => {
+    const res = await fetch(`http://localhost:8080/api/advisory/${idProfesional}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "appication/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+    const data = await res.json();
+    setAsesorias(data);
+  };
+
+  const cambiarEstado = () => {
+    setEstado(true);
+    setTimeout(() => {
+      setEstado(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    obtenerAsesorias(localStorage.getItem("idProfesional"));
+    cambiarEstado();
+  }, []);
+
+  return (
+    <>
+      {estado ? (
+        <Loading />
+      ) : (
+        <div className="container">
+          <div>
+            <h2 className="center">Actializar asesoria</h2>
+          </div>
+          <Badges asesorias={asesorias} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UpdateAdvisory;
---
+++ src/components/Utils/Badges/index.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import M from "materialize-css";
+
+const Badges = ({ asesorias }) => {
+  const cancelarAsesoria = async (idAsesoria) => {
+    try {
+      const res = await fetch("http://localhost:8080/api/update-advisory", {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token"),
+        },
+        body: JSON.stringify({
+          id: idAsesoria,
+          estado: "Cancelada",
+        }),
+      });
+      alert(res);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    const elem = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(elem);
+  });
+
+  return (
+    <div className="row">
+      <ul className="collapsible">
+        {asesorias.map((asesoria) => (
+          <li>
+            <div className="collapsible-header">{asesoria.nombre}</div>
+            <div className="collapsible-body">
+              <div className="input-field">
+                {asesoria.estado === "Cancelada" ? null : (
+                  <button
+                    className="btn red right"
+                    onClick={() => cancelarAsesoria(asesoria.id)}
+                  >
+                    Cancelar asesoria
+                  </button>
+                )}
+                <p>ID Asesoria: {asesoria.id}</p>
+                <p>Numero: {asesoria.numero}</p>
+                <p>Nombre empresa: {asesoria.nombreEmpresa}</p>
+                <p>Nombre profesional: {asesoria.nombreProfesional}</p>
+                <p>Valor: {asesoria.valor}</p>
+                <p>Estado: {asesoria.estado}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Badges;
