62a7d6b26b41f95b65c7fcd941e40b2e7d42cea7
Sebastian Lara <pelukdev@gmail.com>
2022-01-28 21:38:30 -0300

    formulario creacion de asesoria

--- src/components/Professional/Advisory/CreateAdvisory/index.jsx
+++ src/components/Professional/Advisory/CreateAdvisory/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import DatePicker from "../../../Utils/DatePicker";
+import Input from "../../../Utils/Input";
 import Loading from "../../../Utils/Loading";
 import Select from "../../../Utils/Select";
+import Textarea from "../../../Utils/Textarea";
 
 const CreateAdvisory = () => {
   const [clientes, setClientes] = useState();
   const [estado, setEstado] = useState(true);
 
   const obtenerClientes = async () => {
-    const res = await fetch("http://localhost:8080/api/all-client", {
+    const res = await fetch("http://localhost:8080/api/clients", {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -28,16 +31,29 @@ const CreateAdvisory = () => {
     }, 3000);
   };
 
-  const tipoVisita = [
-    {
-      id: 1,
-      nombre: "normal",
-    },
-    {
-      id: 2,
-      nombre: "especial",
-    },
-  ];
+  const crearAsesoria = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:8080/api/create-advisory", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        nombre: document.getElementById("nombreAsesoria").value,
+        descChecklist: document.getElementById("checklist").value,
+        fechaCreacion: new Date(document.getElementById("fechaCreacion").value),
+        fechaLimite: new Date(document.getElementById("fechaLimite").value),
+        fechaTermino: new Date(document.getElementById("fechaTermino").value),
+        valor: document.getElementById("valor").value,
+        idClienteFk: document.getElementById("cliente").value,
+        idProfesionalFk: localStorage.getItem("idProfesional"),
+      }),
+    });
+    alert(res);
+    window.location.href = "/profile";
+  };
 
   useEffect(() => {
     obtenerClientes();
@@ -51,13 +67,33 @@ const CreateAdvisory = () => {
       ) : (
         <div className="container">
           <div>
-            <h2 className="center">Crear capacitacion</h2>
+            <h2 className="center">Crear Asesoria</h2>
           </div>
           {/** Foremulario de creacion de capacitacion */}
-          <div>
+          <div className="row">
             <form>
-              <Select data={clientes} label="Clientes" id="cliente" />
-              <Select data={tipoVisita} label="Tipo visita" id="tipoVisita" />
+              <div>
+                <Input id="nombreAsesoria" label="Nombre Asesoria" />
+                <Select data={clientes} label="Clientes" id="cliente" />
+                {/* <Select data={tipoVisita} label="Tipo visita" id="tipoVisita" /> */}
+                <DatePicker label="Fecha de creacion" id="fechaCreacion" />
+                <DatePicker label="Fecha limite" id="fechaLimite" />
+                <DatePicker label="Fecha de termino" id="fechaTermino" />
+                <Textarea id="checklist" label="Checklist" />
+                <Input id="valor" label="Valor" />
+              </div>
+              <div>
+                <button className="btn right" onClick={crearAsesoria}>
+                  <i className="material-icons right">send</i>
+                  Crear
+                </button>
+              </div>
+              <div>
+                <a href="#!" className="btn">
+                  <i className="material-icons left">arrow_back</i>
+                  Volver
+                </a>
+              </div>
             </form>
           </div>
         </div>
